1c3ef8050c5766d970d89092282696a280d5b1a7
Rob Brackett <rob@robbrackett.com>
2012-04-19 17:57:02 -0700

    First Commit

---
+++ demo/demo.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // config
+  flickrAbout.API_KEY = "1b62c2260cbfac785aede9c3ab902af5";
+  flickrAbout.PREFERRED_WIDTH = screen.width;
+  
+  window.doSearch = function() {
+    var searchTerm = document.getElementById("search-field").value,
+        woeId = document.getElementById("search-in-field").value;
+    
+    var handler = function (info) {
+      handleFindResults(info, {term: searchTerm, woeId: woeId});
+    };
+    
+    if (woeId === "[nearby]") {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        flickrAbout.find(searchTerm, handler, {location: [position.coords.latitude, position.coords.longitude]});
+      });
+    }
+    else {
+      flickrAbout.find(searchTerm, handler, {woe_id: woeId});
+    }
+  };
+  
+  var setLoaderVisible = function(visible) {
+    if (visible) {
+      document.getElementById("loader").style.display = "inline";
+      document.getElementById("search-button").style.display = "none";
+    }
+    else {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("search-button").style.display = "";
+    }
+  }
+  
+  var handleFindResults = function(info, search) {
+    if (info == null) {
+      setLoaderVisible(false);
+      alert("No matching photos were found :(");
+      return;
+    }
+    
+    var figure = document.getElementById("found-image"),
+        img = figure.getElementsByTagName("img")[0],
+        caption = figure.getElementsByTagName("figcaption")[0];
+    
+    var showImage = function () {
+      var profileUrl = "http://www.flickr.com/people/" + info.username + "/";
+      caption.innerHTML = "<a href=\"" + info.flickrPage + "\">Photo</a> by <a href=\"" + profileUrl + "\">" + info.owner + "</a> on Flickr";
+      img.src = info.source;
+
+      img.style.display = "none";
+      figure.style.background = "url(" + info.source + ")";
+      if (info.width > info.height) {
+        img.style.height = "100%";
+        img.style.width = "";
+      }
+      else {
+        img.style.height = "";
+        img.style.width = "100%";
+      }
+      
+      setLoaderVisible(false);
+
+      // update location
+      window.location = window.location.href.match(/^[^?#]*/)[0] + "#" + 
+        encodeURIComponent(search.term) + "/" + 
+        encodeURIComponent(search.woeId) + "/" + 
+        encodeURIComponent(info.id);
+    };
+    
+    var loader = new Image();
+    loader.src = info.source;
+    if (loader.complete) {
+      showImage();
+    }
+    else {
+      loader.addEventListener("load", showImage, false);
+    }
+  };
+  
+  // actual event
+  var form = document.getElementById("search-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    doSearch();
+    setLoaderVisible(true);
+  }, false);
+  
+  // geo support
+  if (navigator.geolocation) {
+    // add option
+    var option = document.createElement("option");
+    option.value = "[nearby]";
+    option.appendChild(document.createTextNode("Near you!"));
+    document.getElementById("search-in-field").appendChild(option);
+  }
+  
+  // initial criteria
+  var hashParts = window.location.hash.slice(1).split("/");
+  if (window.location.hash && hashParts[0]) {
+    document.getElementById("search-field").value = decodeURIComponent(hashParts[0]);
+    document.getElementById("search-in-field").value = decodeURIComponent(hashParts[1]) || "";
+  }
+  
+  // start off with something
+  if (hashParts[2]) {
+    flickrAbout.getPhoto(decodeURIComponent(hashParts[2]), function(info) {
+      handleFindResults(info, {term: decodeURIComponent(hashParts[0]), woeId: decodeURIComponent(hashParts[1]) || ""});
+    });
+  }
+  else {
+    doSearch();
+  }
+}, false);
---
+++ src/FlickrAbout.js
@@ -0,0 +1,156 @@
+var flickrAbout = {
+  // Feel free to set these
+  API_KEY: "[no key]",
+  PREFERRED_WIDTH: 1000,
+  
+  // but not these
+  BASE_URL: "http://api.flickr.com/services/rest/?",
+  
+  _searchCache: {},
+  _imageInfo: {},
+  
+  find: function (term, callback, options) {
+    // basic params for a search
+    var search = {
+      method: "flickr.photos.search",
+      text: term,
+      content_type: 1,
+      license: "1,2,3,4,5,6,7,8"
+    };
+    
+    // add any location bits
+    var locationKey = "";
+    if (options.woe_id) {
+      search.woe_id = options.woe_id;
+      locationKey = options.woe_id;
+    }
+    else if (options.location) {
+      search.lat = options.location[0];
+      search.lon = options.location[1];
+      search.radius = options.radius || 25;
+      locationKey = options.location.join(",");
+    }
+    
+    // pull from cache or make the request
+    var cacheKey = term + ":" + locationKey;
+    if (this._searchCache[cacheKey]) {
+      this._handleSearchResults(this._searchCache[cacheKey], search, callback);
+    }
+    else {
+      this.makeRequest(search, function (data) {
+        var photos = data.photos.photo;
+        this._searchCache[cacheKey] = photos;
+        this._handleSearchResults(photos, search, callback);
+      });
+    }
+  },
+  
+  getPhoto: function (id, callback) {
+    if (this._imageInfo[id]) {
+      this._checkDone(this._imageInfo[id], callback);
+    }
+    else {
+      this._imageInfo[id] = {id: id};
+      this.makeRequest({
+        method: "flickr.photos.getinfo",
+        photo_id: id
+      }, this._getInfoCallback(id, callback));
+      this.makeRequest({
+        method: "flickr.photos.getsizes",
+        photo_id: id
+      }, this._getSizesCallback(id, callback));
+    }
+  },
+  
+  makeRequest: function (params, callback) {
+    params.api_key = this.API_KEY;
+    params.format = "json";
+    this.jsonp.request(this.BASE_URL, params, callback, this);
+  },
+  
+  _getInfoCallback: function (photoId, callback) {
+    return function (data) {
+      console.log("Info: ", data);
+      var info = this._imageInfo[data.photo.id];
+      info._infoComplete = true;
+      info.owner = data.photo.owner.realname || data.photo.owner.username;
+      info.realname = data.photo.owner.realname;
+      info.username = data.photo.owner.username;
+      info.flickrPage = data.photo.urls.url[0]._content;
+      this._checkDone(info, callback);
+    };
+  },
+  
+  _getSizesCallback: function (photoId, callback) {
+    return function (data) {
+      console.log("Sizes: ", data);
+      var info = this._imageInfo[photoId];
+      info._sizesComplete = true;
+      var sizes = data.sizes.size;
+      var match = sizes[sizes.length - 1];
+      for (var i = 0; i < sizes.length; i++) {
+        if (sizes[i].width >= this.PREFERRED_WIDTH) {
+          match = sizes[i];
+          break;
+        }
+      }
+
+      info.source = match.source;
+      info.width = match.width;
+      info.height = match.height;
+      this._checkDone(info, callback);
+    };
+  },
+  
+  _checkDone: function (info, callback) {
+    if (info._infoComplete && info._sizesComplete && callback) {
+      callback.call(null, info);
+    }
+  },
+  
+  _handleSearchResults: function (photos, search, callback) {
+    // bail if there were no results
+    if (!photos.length) {
+      callback && callback.call(null, null);
+      return;
+    }
+    // pick one from the list randomly
+    var index = Math.floor(Math.random() * photos.length);
+    var id = photos[index].id;
+    this.getPhoto(id, callback);
+  },
+  
+  jsonp: {
+    CALL_PATH: "flickrAbout.jsonp.",
+    
+    requestId: 0,
+    
+    request: function (url, params, callback, context) {
+      var callbackKey = "jsoncallback";
+      params = params || {};
+      var callbackId = "callback" + this.requestId;
+      this.requestId++;
+      params[callbackKey] = this.CALL_PATH + callbackId;
+      
+      this[callbackId] = function (data) {
+        delete this[callbackId];
+        script.parentNode.removeChild(script);
+        callback.apply(context, arguments);
+      };
+      
+      var urlParts = [];
+      for (var key in params) {
+        urlParts.push(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
+      }
+      if (!~url.indexOf("?")) {
+        url += "?";
+      }
+      url += urlParts.join("&");
+
+      var script = document.createElement("script");
+      script.setAttribute("type", "text/javascript");
+      script.setAttribute("src", url);
+      document.body.appendChild(script);
+    }
+  }
+};
